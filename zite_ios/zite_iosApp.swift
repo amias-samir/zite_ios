82ce17379619c9accbe75fde706a4d54dab28a33
amias-samir <samirdangal@gmail.com>
2026-06-01 17:13:33 +0545

    Initial Commit

---
+++ zite_ios/zite_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  zite_iosApp.swift
+//  zite_ios
+//
+//  Created by Amias on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct zite_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
